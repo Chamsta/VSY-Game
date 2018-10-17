57e188ffc4aaea2a3454c9e2b291654234a8ed99
ebrs <ebrs@10.13.70.48>
2018-10-17 09:16:44 +0200

    Punkte weg

--- Server/src/main/Main.java
+++ Server/src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Server startet. Wenn es mal programmiert wurde...");
+		System.out.println("Server startet. Wenn es mal programmiert wurde.");
 	}
 
 }
