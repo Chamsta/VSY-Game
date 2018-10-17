20bbd69f247331e77bf1d3db05faa19c3d206506
Julius <julius.vomwege@online.de>
2018-10-17 09:49:35 +0200

    Client erstellt

---
+++ Client/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Ich bin dein Client.");
+	}
+
+}
