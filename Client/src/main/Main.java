a0ef4e9f65ac3e39c5f1e623342d23741866e196
Julius <julius.vomwege@online.de>
2018-10-24 16:05:23 +0200

    Echo Server implementiert.

--- Client/src/main/Main.java
+++ Client/src/main/Main.java
@@ -1,11 +1,28 @@
 package main;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 import javax.swing.JOptionPane;
+
+import intface.Echo;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		JOptionPane.showMessageDialog(null, "Ich bin dein Client.");
+		String host = (args.length < 1) ? null : args[0];
+		System.out.println(host);
+        try {
+            Registry registry = LocateRegistry.getRegistry(host);
+            Echo stub = (Echo) registry.lookup("Echo");
+            String response = stub.echoThis("Hallo Du da.");
+            System.out.println("response: " + response);
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
 	}
 
 }
--- Server/src/main/Main.java
+++ Server/src/main/Main.java
@@ -1,9 +1,34 @@
 package main;
 
-public class Main {
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+import intface.Echo;
+
+
+public class Main implements Echo{
 
 	public static void main(String[] args) {
 		System.out.println("Server startet. Wenn es mal programmiert wurde...");
+		try {
+			Main obj = new Main();
+			Echo stub = (Echo) UnicastRemoteObject.exportObject(obj, 0);
+
+            // Bind the remote object's stub in the registry
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("Echo", stub);
+
+            System.err.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
 	}
 
+	@Override
+	public String echoThis(String text) throws RemoteException {
+		return "Echo " + text;
+	}
 }
