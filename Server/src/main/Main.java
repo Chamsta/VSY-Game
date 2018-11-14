73ad047ab38c99788a7f3938eae405e5a4f92324
Julius <julius.vomwege@online.de>
2018-11-14 09:22:19 +0100

    Verbindung zu vom-Wege.de funtktioniert!!!!

--- Server/src/main/Main.java
+++ Server/src/main/Main.java
@@ -13,7 +13,15 @@ public class Main implements Echo{
 
 	public static void main(String[] args) {
 		System.out.println("Server startet. Wenn es mal programmiert wurde...");
+		String host = (args.length < 1) ? null : args[0];
+		if(host == null){
+			System.out.println("Bitte Adresse des Servers beim Start angeben!");
+			System.exit(0);
+		}
+		System.out.println("Server startet auf Adresse: " + host);
 		try {
+			System.setSecurityManager(new SecurityManager());
+			System.setProperty("java.rmi.server.hostname",host);
 			Main obj = new Main();
 			Echo stub = (Echo) UnicastRemoteObject.exportObject(obj, 0);
 
@@ -21,7 +29,7 @@ public class Main implements Echo{
             registry = LocateRegistry.getRegistry();
             registry.bind("Echo", stub);
 
-            System.err.println("Server ready");
+            System.err.println("Server ready!");
         } catch (Exception e) {
             System.err.println("Server exception: " + e.toString());
             e.printStackTrace();
