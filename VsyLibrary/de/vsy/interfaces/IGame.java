5f1ba09fd9da91b4a02488fd0cd02cb9b06f65a5
Gladis NDOUAB'S <gladis.douabaletmaika@stud.hn.de>
2019-03-13 22:53:57 +0100

    Describes the Java docs for others interfaces

--- VsyLibrary/de/vsy/interfaces/IGame.java
+++ VsyLibrary/de/vsy/interfaces/IGame.java
@@ -7,81 +7,130 @@ import java.util.HashMap;
 
 import de.vsy.interfaces.tictactoe.GameStatus;
 
+/**
+ * The interface to communicate with remote game.
+ */
 public interface IGame extends Remote, Serializable{
 
 	/**
+	 * Gets the list of cells.
 	 * @return the cells
 	 */
 	HashMap<String, Boolean> getCells() throws RemoteException;
 
+	/**
+	 * Gets the cell value on specified position.
+	 * @param positionX The column position.
+	 * @param positionY The row position.
+	 * @return the cells
+	 */
 	Boolean getCell(int positionX, int positionY) throws RemoteException;
 
+	/**
+	 * Gets the cell value of specified key position.
+	 * @param key The key coordinate.
+	 * @return the cell value.
+	 */
 	Boolean getCell(String key) throws RemoteException;
 
 	/**
-	 * @param cells the cells to set
+	 * Sets the cell value on specified position.
+	 * @param positionX The column position.
+	 * @param positionY The row position.
+	 * @param value the cell value to set.
+	 * @return the cell value saved.
 	 */
-
 	Boolean setCell(int positionX, int positionY, Boolean value) throws RemoteException;
 
+	/**
+	 * Sets the cell value of specified key position.
+	 * @param key The key position.
+	 * @param value the cell value to set.
+	 * @return the cell value saved.
+	 */
 	Boolean setCell(String key, Boolean value) throws RemoteException;
 	
+	/**
+	 * Sets the cell for the specified player.
+	 * @param key The key coordinate.
+	 * @param player The player name.
+	 * @return the cell value saved.
+	 */
 	Boolean setCell(String key, String player) throws RemoteException;
 
 	/**
-	 * @return the gameSize
+	 * Gets the size of game.
+	 * @return the game size.
 	 */
 	int getGameSize() throws RemoteException;
 
 	/**
-	 * @return the nextPlayer
+	 * Gets the player that playing next.
+	 * @return the player is playing next.
 	 */
 	String getNextPlayer() throws RemoteException;
 
 	/**
-	 * @return the nextPlayer
+	 * Gets the winner of this game.
+	 * @return the winner name of this game.
 	 */
 	String getWinner() throws RemoteException;
 
 	/**
-	 * @param nextPlayer the nextPlayer to set
+	 * Sets the next player.
+	 * @param nextPlayer the player name to set
 	 */
 	void setNextPlayer(String nextPlayer) throws RemoteException;
 
 	/**
-	 * @return the player1
+	 * Gets the first player.
+	 * @return the first player.
 	 */
 	String getPlayer1() throws RemoteException;
 
 	/**
-	 * @param player1 the player1 to set
+	 * Sets the first player to the game.
+	 * @param value the player name to set
 	 */
 	void setPlayer1(String value) throws RemoteException;
 
 	/**
-	 * @return the player2
+	 * Gets the second player.
+	 * @return the second player.
 	 */
 	String getPlayer2() throws RemoteException;
 
 	/**
-	 * @param player2 the player2 to set
+	 * Sets the second player to the game.
+	 * @param value the player name to set
 	 */
 	void setPlayer2(String value) throws RemoteException;
 
 	/**
+	 * Gets the game status.
 	 * @return the status
 	 */
 	GameStatus getStatus() throws RemoteException;
 
 	/**
+	 * Sets the game status.
 	 * @param status the status to set
 	 */
 	void setStatus(GameStatus status) throws RemoteException;
 
+	/**
+	 * Starts the game.
+	 */
 	void Play() throws RemoteException;
 
+	/**
+	 * Stops the game.
+	 */
 	void Stop() throws RemoteException;
 
+	/**
+	 * Switches the player that playing next.
+	 */
 	void SwitchPlayer() throws RemoteException;
 	
 	/**
--- VsyLibrary/de/vsy/interfaces/IServer.java
+++ VsyLibrary/de/vsy/interfaces/IServer.java
@@ -4,13 +4,55 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
 
+/**
+ * The interface to communicate with remote server.
+ */
 public interface IServer extends Remote{
+	/**
+	 * Logs in the player into the server.
+	 * @param user The player name to log in.
+	 */
 	void login(String user) throws RemoteException;
+	
+	/**
+	 * Gets the game identification for an user.
+	 * @param user The player name
+	 */
 	int getGameId(String user) throws RemoteException;
+		
+	/**
+	 * Logs out the player from the server.
+	 * @param user The player name to log out.
+	 */
 	void logout(String user) throws RemoteException;
+		
+	/**
+	 * Adds the client game for updating the game board
+	 * @param gameId The game identification.
+	 * @param user The player for this client game.
+	 * @param clientGame The client game.
+	 */
 	void addClientGame(int gameId, String user, IGame clientGame) throws RemoteException;
+		
+	/**
+	 * Gets the list of servers.
+	 * @return The servers list.
+	 */
 	List<String> getServerList() throws RemoteException;
+		
+	/**
+	 * Ping the server if it is available.
+	 * @return true if it is available, otherwise false.
+	 */
 	boolean ping() throws RemoteException;
+		
+	/**
+	 * Checks all servers if they are available.
+	 */
 	void checkServers() throws RemoteException;
+		
+	/**
+	 * Notify all clients.
+	 */
 	void informClients() throws RemoteException;
 }
